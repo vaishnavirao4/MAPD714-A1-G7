7e1f34ca2df3ffb78c0cf1d7826a9718b59ebdaa
Kisu <krishna@Krishnas-MacBook-Air.local>
2022-10-22 18:59:45 -0400

    landscape mode done

---
+++ Calculator/AppDelegate.swift
@@ -0,0 +1,47 @@
+/**App Name: Calculator
+Version: 2.0
+Created on: 08-10-2022
+ 
+ 
+Created by:
+ 
+ 
+Krishna Patel 301268911
+Vaishnavi Santhapuri 301307031
+Kowndinya Varanasi 301210621
+ 
+
+
+Description:
+This is a Calculator App that will be used to calculate the basic
+arithmetic operations such as addition, subtraction, multiplication, division and percentages.  */
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,173 @@
+/**App Name: Calculator
+Version: 2.0
+Created on: 08-10-2022
+ 
+ 
+Created by:
+ 
+ 
+Krishna Patel 301268911
+Vaishnavi Santhapuri 301307031
+Kowndinya Varanasi 301210621
+ 
+
+
+Description:
+This is a Calculator App that will be used to calculate the basic
+arithmetic operations such as addition, subtraction, multiplication, division and percentages.  */
+
+import UIKit
+
+enum Operation:String {
+    case Add = "+"
+    case Subtract = "-"
+    case Divide = "/"
+    case Multiply = "*"
+    case NULL = "NULL"
+    
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    var currentNumber = ""
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
+    var currentOperation:Operation = .NULL
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Loads the view onto the screen
+        resultLbl.text = "0"
+    }
+    
+    // This is the function for the numbered button
+    @IBAction func NumberBtnPressed(_ sender: UIButton) {
+        if currentNumber.count <= 9
+        {
+            currentNumber += "\(sender.tag)"
+            resultLbl.text = currentNumber
+        }
+    }
+    
+    // This is the function for backspace button
+    @IBAction func BackSpaceBtnPessed(_ sender: UIButton) {
+            if (!resultLbl.text!.isEmpty)
+            {
+                if (resultLbl.text!.count == 1)
+                {
+                    resultLbl.text = "0"
+                }
+                else
+                {
+                    resultLbl.text?.removeLast()
+                }
+            }
+        }
+    
+        // This is the function for clear button
+    @IBAction func ClearBtnPressed(_ sender: UIButton) {
+        currentNumber = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .NULL
+        resultLbl.text = "0"
+    }
+    
+        // This is the function for decimal button
+    @IBAction func DotBtnPressed(_ sender: UIButton) {
+        if currentNumber.count <= 7
+        {
+            currentNumber += "."
+            resultLbl.text = currentNumber
+        }
+    }
+    
+    // This is the function for equal button
+    @IBAction func EqualBtnPressed(_ sender: UIButton) {
+        operation(operation: currentOperation)
+    }
+    
+    // This is the function for addition button
+    @IBAction func AddBtnPressed(_ sender: UIButton) {
+        operation(operation: .Add)
+    }
+    
+    // This is the function for subtraction button
+    @IBAction func SubtractBtnPressed(_ sender: UIButton) {
+        operation(operation: .Subtract)
+    }
+    
+    // This is the function for multiplication button
+    @IBAction func MultiplyBtnPressed(_ sender: UIButton) {
+        operation(operation: .Multiply)
+    }
+    
+    // This is the function for division button
+    @IBAction func DivideBtnPressed(_ sender: UIButton) {
+        operation(operation: .Divide)
+    }
+    
+    // This is the function for percentage button
+    @IBAction func PercentageBtnPressed(_ sender: UIButton) {
+        currentNumber = "\(Double(currentNumber)! / 100 )"
+        result = currentNumber
+        resultLbl.text = result
+    }
+    
+    // This is the function for toggling between the negative and positive value
+    @IBAction func PlusMinusBtnPressed(_ sender: UIButton) {
+            currentNumber = "\(Int(currentNumber)! * -1)"
+            result = currentNumber
+            resultLbl.text = result
+        
+    }
+    
+    // This is the function when we make a selection of arithmetic operators
+    func operation(operation: Operation) {
+        if currentOperation != .NULL
+        {
+            if currentNumber != ""
+            {
+                rightValue = currentNumber
+                currentNumber = ""
+                
+                if currentOperation == .Add
+                {
+                    result = "\(Double(leftValue)! + Double(rightValue)!)"
+                }
+                else if currentOperation == .Subtract
+                {
+                    result = "\(Double(leftValue)! - Double(rightValue)!)"
+                }
+                else if currentOperation == .Multiply
+                {
+                    result = "\(Double(leftValue)! * Double(rightValue)!)"
+                }
+                else if currentOperation == .Divide
+                {
+                    result = String(format:"%.8f",((Double(leftValue)! / Double(rightValue)!)))
+                }
+              
+                leftValue = result
+                if (Double(result)!.truncatingRemainder(dividingBy: 1) == 0)
+                {
+                    result = "\(Int(Double(result)!))"
+                }
+                resultLbl.text = result
+            }
+            currentOperation = operation
+        }
+        else
+        {
+            leftValue = currentNumber
+            currentNumber = ""
+            currentOperation = operation
+        }
+    }
+    
+}
